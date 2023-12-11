47ee6a8e504dd84f8c2108ebdcda6fdac2aea31c
sanjaymaraboina <maraboinasanjay.careerpedia@gmail.com>
2023-12-11 22:46:25 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,94 @@
+let totalImages = document.querySelector(".images");
+let image = document.querySelector(".image");
+let imageOf = document.querySelector(".image1");
+let imageOf1 = document.querySelector(".image2");
+let imageOf2 = document.querySelector(".image3");
+let imageOf3 = document.querySelector(".image4");
+let imageOf4 = document.querySelector(".image5");
+let imageOf5 = document.querySelector(".image6");
+let imageOf6 = document.querySelector(".image7");
+let imageOf7 = document.querySelector(".image8");
+let cross = document.querySelector(".close");
+let line1 = document.querySelector(".line1");
+let line2 = document.querySelector(".line2");
+
+
+
+
+
+function imageSize() {
+  imageOf.style.display = "block";
+ 
+  totalImages.style.opacity = "0.1";
+  cross.style.display = "block";
+}
+
+function imageSize1(){
+  imageOf1.style.display = "block";
+totalImages.style.opacity = "0.1";
+cross.style.display = "block";
+
+}
+function imageSize2(){
+  imageOf2.style.display = "block";
+totalImages.style.opacity = "0.1";
+cross.style.display = "block";
+
+}
+function imageSize3(){
+  imageOf3.style.display = "block";
+totalImages.style.opacity = "0.1";
+cross.style.display = "block";
+
+}
+function imageSize4(){
+  imageOf4.style.display = "block";
+totalImages.style.opacity = "0.1";
+cross.style.display = "block";
+
+}
+function imageSize5(){
+  imageOf5.style.display = "block";
+totalImages.style.opacity = "0.1";
+cross.style.display = "block";
+
+}
+function imageSize6(){
+  imageOf6.style.display = "block";
+totalImages.style.opacity = "0.1";
+cross.style.display = "block";
+
+}
+function imageSize7(){
+  imageOf7.style.display = "block";
+totalImages.style.opacity = "0.1";
+cross.style.display = "block";
+
+}
+
+
+let burger = document.getElementsByClassName("close");
+
+function intoMark() {
+  imageOf.style.display = "none";
+  imageOf1.style.display = "none";
+  imageOf2.style.display = "none";
+  imageOf3.style.display = "none";
+  imageOf4.style.display = "none";
+  imageOf5.style.display = "none";
+  imageOf6.style.display = "none";
+  imageOf7.style.display = "none";
+  totalImages.style.opacity = "1";
+}
+
+
+
+
+
+// imageOf1.style.display = "block";
+// imageOf2.style.display = "block";
+// imageOf3.style.display = "block";
+// imageOf4.style.display = "block";
+// imageOf5.style.display = "block";
+// imageOf6.style.display = "block";
+// imageOf7.style.display = "block";
